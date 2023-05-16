612ad6f47185b9e0f5e53bf8a2fc0655a1b16bf4
grigormagic <67559768+grigormagic@users.noreply.github.com>
2023-05-16 22:36:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let button = document.getElementById("button");
+let textarea = document.getElementById("textarea");
+let active;
+const recognition = new webkitSpeechRecognition();
+recognition.lang = "en-US";
+recognition.continuous = true;
+reset();
+recognition.onend = reset();
+
+recognition.onresult = function (event) {
+  for (let i = event.resultIndex; i < event.results.length; i++) {
+    if (event.results[i].isFinal) {
+      textarea.value += event.results[i][0].transcript;
+    }
+  }
+};
+
+function reset() {
+  active = false;
+  button.innerHTML = "Click to Speak";
+}
+
+function StartStopBtn() {
+  if (active) {
+    recognition.stop();
+    reset();
+  } else {
+    recognition.start();
+    active = true;
+    button.innerHTML = "Click to Stop";
+  }
+}
+
+button.addEventListener("click", StartStopBtn);
